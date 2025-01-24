c1cae45dcf500e6a03db01f92825b4aa74087f9c
Evan Matherne <evanmatherne55@gmail.com>
2025-01-24 15:56:26 -0600

    Liquid weighs down container

--- Assets/pipetteScript.cs
+++ Assets/pipetteScript.cs
@@ -6,7 +6,6 @@ public class pipetteScript : MonoBehaviour
     public float checkTimeOut = 0.2f; float timeOfNextCheck;
     public float flowSpeed = 3f;
     public ObiEmitter emitter;
-    public Collider floor;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,7 +13,6 @@ public class pipetteScript : MonoBehaviour
     {   
         timeOfNextCheck = Time.time + checkTimeOut;
         emitter = transform.Find("Tip").GetComponent<ObiEmitter>();
-        Debug.Log(emitter.solver.activeParticles);
 
     }
 
@@ -31,12 +29,7 @@ public class pipetteScript : MonoBehaviour
                 Vector4 particlePosition = thissolver.positions[particleIndex];
                 Vector3 worldPosition = emitter.solver.transform.TransformPoint(particlePosition);
 
-                Debug.Log($"Particle {particleIndex} is at world position: {worldPosition}");
-
-                if (floor.bounds.Contains(worldPosition))
-            {
-                thissolver.life[particleIndex] = 0;
-            }
+                if (worldPosition.y <= 0.2f) emitter.solver.life[particleIndex] = 0f;
             }
 
         }
