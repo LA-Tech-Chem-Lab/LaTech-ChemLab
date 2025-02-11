e64373df58d1f932390f718827e420bc0ea7ddfe
Masonkta <88817850+Masonkta@users.noreply.github.com>
2025-02-11 15:38:06 -0600

    New UI Scenes

---
+++ Assets/00 Scripts/Scene Change.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Function to change the scene by name
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Function to change the scene by index
+    public void ChangeSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
