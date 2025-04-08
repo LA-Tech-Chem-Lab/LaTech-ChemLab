521b9f81654604b361070a754738551d1877472b
Evan Matherne <58615631+CarlRox55@users.noreply.github.com>
2025-04-07 22:47:19 -0500

    Audio For Shower and Eye Wash

--- Assets/00 Scripts/EyeWashStationScript.cs
+++ Assets/00 Scripts/EyeWashStationScript.cs
@@ -17,10 +17,14 @@ public class EyeWashStationScript : MonoBehaviour
     public ObiEmitter eyeEmitterL;
     public ObiEmitter eyeEmitterR;
 
+    
+    public AudioSource showerAudioSrc;
+    public AudioSource eyeWashAudioSrc;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        showerAudioSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,9 +35,23 @@ public class EyeWashStationScript : MonoBehaviour
         showerHeadEmitter.speed = leverPulled ? showerEmitterSpeed : 0f;
         pullLever.transform.rotation = Quaternion.Euler(new Vector3(0, -90f, 0));
 
+        if (leverPulled)
+            if (!showerAudioSrc.isPlaying)
+                showerAudioSrc.Play();
+                
+        if (!leverPulled)
+            showerAudioSrc.Stop();
+
         // Eye Wash
         handlePushed = !handleDoorScript.doorIsClosed;
         eyeEmitterL.speed = handlePushed ? eyeWashEmitterSpeed : 0f;
         eyeEmitterR.speed = handlePushed ? eyeWashEmitterSpeed : 0f;
+
+        if (handlePushed)
+            if (!eyeWashAudioSrc.isPlaying)
+                eyeWashAudioSrc.Play();
+                
+        if (!handlePushed)
+            eyeWashAudioSrc.Stop();
     }
 }
